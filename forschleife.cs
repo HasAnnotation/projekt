ab731eb7f17e80534c60597d01a78222dd05d923
Hasan Kara <hasan9675.hk@gmail.com>
2026-04-24 13:29:36 +0200

    Add files via upload

---
+++ forschleife.cs
@@ -0,0 +1,26 @@
+namespace forschleife
+{
+    internal class Program
+    {
+
+        static void Main (string[]args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            string[] cars = {"bmw", "mercedes", "audi"};
+
+            foreach (string car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+            
+        }
+
+    
+        
+    }    
+}

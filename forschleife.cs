61b602280d2c61d6fd17cb49a6012cd7342e7d5c
hasan <hasan9675.hk@gmail.com>
2026-04-24 14:23:30 +0200

    testg comment

--- forschleife.cs
+++ forschleife.cs
@@ -3,24 +3,24 @@ namespace forschleife
     internal class Program
     {
 
-        static void Main (string[]args)
+        static void Main(string[] args)
         {
             for (int i = 0; i <= 10; i++)
             {
                 Console.WriteLine(i);
             }
 
-            string[] cars = {"bmw", "mercedes", "audi"};
+            string[] cars = { "bmw", "mercedes", "audi" };
 
             foreach (string car in cars)
             {
                 Console.WriteLine(car);
             }
+            //askjbdfkasdjb
 
-            
         }
 
-    
-        
-    }    
+
+
+    }
 }
